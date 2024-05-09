62083ec48b503f5ef8d728c15d0c296de4b187c7
Shariar Islam <shariarn85@gmail.com>
2024-05-09 22:02:35 +0600

    Insert,InsertOne, Find, FindOne, Field Filtering, Project

--- index.ts
+++ index.ts
@@ -5,4 +5,11 @@
  * Mongosh Shell downloaded to use in windows terminal
  * 
  * used command like: create db, insert etc
+ * 
+ * 
+ * 
+ * Filed Filtering: db.test.find({age: 17}, {gender: 1, name:1, company: 1, email: 1})
+ * 
+ * Project: db.test.find({age: 17}).project({name: 1, email:1, gender: 1})
+ * project doesn't works with findOne.
  *  */
