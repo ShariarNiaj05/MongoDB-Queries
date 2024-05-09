7743b7efb805741f31cc6f76be02b09336fc7ff5
Shariar Islam <shariarn85@gmail.com>
2024-05-09 22:13:50 +0600

    , , , , ,

--- index.ts
+++ index.ts
@@ -13,3 +13,14 @@
  * Project: db.test.find({age: 17}).project({name: 1, email:1, gender: 1})
  * project doesn't works with findOne.
  *  */
+
+/***
+ * 5-3 
+ * $Eq =     db.test.find({age: {$eq: 17}}), 
+ * $Neq =     db.test.find({age: {$ne: 17}}), 
+ * $Gt =     db.test.find({age: {$gt: 17}}), 
+ * $Lt =     db.test.find({age: {$lt: 17}}), 
+ * $Gte =     db.test.find({age: {$gte: 30}}), 
+ * $Lte =     db.test.find({age: {$lte: 45}})
+ * 
+ */ 
