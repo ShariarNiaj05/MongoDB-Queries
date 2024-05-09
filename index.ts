d5019ab60870e14f534634d66e8a55238f30495d
Shariar Islam <shariarn85@gmail.com>
2024-05-09 20:02:31 +0600

    Install MongoDB Compass

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("MongoDB-Queries");
+// 5-1-A Install MongoDB Compass & No SQL Booster ( Windows)
+
+/****
+ * Mongodb Compass downloaded
+ * Mongosh Shell downloaded to use in windows terminal
+ * 
+ * used command like: create db, insert etc
+ *  */
